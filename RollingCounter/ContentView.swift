56bc42025fe68688f95b087dc11edb94bc414682
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-07 03:54:23 -0500

    Add navigation view

--- RollingCounter/ContentView.swift
+++ RollingCounter/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+
+            }
+            .padding()
+            .navigationTitle("Rolling text")
         }
-        .padding()
     }
 }
 
---
+++ RollingCounter/View/RollingText.swift
@@ -0,0 +1,20 @@
+//
+//  RollingText.swift
+//  RollingCounter
+//
+//  Created by Sergey Lukaschuk on 2022-12-07.
+//
+
+import SwiftUI
+
+struct RollingText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RollingText_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
