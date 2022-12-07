6bc1b89363b40d214b327dc09daff35c25748502
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-07 03:50:57 -0500

    Initial Commit

---
+++ RollingCounter/RollingCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingCounterApp.swift
+//  RollingCounter
+//
+//  Created by Sergey Lukaschuk on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct RollingCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
