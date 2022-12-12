dadb6abfaeeaa9d266f16f75eb5e3a3e7652cf0a
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-12 04:12:20 -0500

    Add animation range

--- RollingCounter/View/RollingText.swift
+++ RollingCounter/View/RollingText.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct RollingText: View {
+    var font: Font = .largeTitle
+    var weight: Font.Weight = .regular
+
+    @Binding var value: Int
+    @State var animationRange: [Int] = []
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            ForEach(0..<animationRange.count, id: \.self) {_ in
+                Text("8")
+                    .font(font)
+                    .fontWeight(weight)
+                    .opacity(0)
+                    .overlay {
+                        GeometryReader { proxy in
+                            let size = proxy.size
+
+                            VStack(spacing: 0) {
+                                ForEach(0...9, id: \.self) { number in
+                                    Text("\(number)")
+                                        .font(font)
+                                        .fontWeight(weight)
+                                        .frame(width: size.width, height: size.height, alignment: .center)
+                                }
+                            }
+                        }
+                        .clipped()
+                    }
+            }
+        }
+        .onAppear {
+            animationRange = Array(repeating: 0, count: "\(value)".count)
+        }
     }
 }
 
