7c9f16874181c3ffc4ff27394abd4f101804255f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-12-15 03:47:09 -0500

    Fix extra range

--- RollingCounter/View/RollingText.swift
+++ RollingCounter/View/RollingText.swift
@@ -43,6 +43,22 @@ struct RollingText: View {
             animationRange = Array(repeating: 0, count: "\(value)".count)
 
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.06) {
+                let extra = "\(value)".count - animationRange.count
+
+                if extra > 0 {
+                    for _ in 0..<extra {
+                        withAnimation(.easeIn(duration: 0.1)) {
+                            animationRange.append(0)
+                        }
+                    }
+                } else {
+                    for _ in 0..<(-extra) {
+                        withAnimation(.easeIn(duration: 0.1)) {
+                            animationRange.removeLast()
+                        }
+                    }
+                }
+
                 updateText()
             }
         }
